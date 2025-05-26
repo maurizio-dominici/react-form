c2097043f17d7e297a564400f5e2dd7fcb077594
maurizio-dominici <mauriziodominici4@gmail.com>
2025-05-26 15:29:02 +0200

    add structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 // IMPORTS
-import reactLogo from "./assets/img/react.svg";
-import viteLogo from "/vite.svg";
-import Header from "./layout/Header";
-import Main from "./layout/Main";
-import Footer from "./layout/Footer";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <h1>react form</h1>
     </>
   );
 }
